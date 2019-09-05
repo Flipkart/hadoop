5f06481e97b4fd36e9c35a7765180d3d1a6efad2
AmithshaS <amithsha.s@flipkart.com>
2019-09-05 09:43:37 +0530

    Cmake symlink

--- buildScripts/buildCmake.sh
+++ buildScripts/buildCmake.sh
@@ -3,5 +3,6 @@
 tar -xvf cmake-3.13.3-Linux-x86_64.tar.gz 
 cd cmake-3.13.3-Linux-x86_64/
 echo "Creating cmake Symlink"
-sudo ln -s bin/cmake /usr/bin/cmake
+echo `pwd`
+sudo ln -s `pwd`/bin/cmake /usr/bin/cmake
 
